f2f1cc7608078b835a063d19254d2ef3c3cc1e5d
yusufkamal-ok <59947329+yusufkamal-ok@users.noreply.github.com>
2024-07-31 21:14:41 +0700

    Add order processing endpoints

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,16 @@ app.get('/products', (req: Request, res: Response) => {
     res.send('List of products');
 });
 
+app.post('/orders', (req: Request, res: Response) => {
+    const { productId, quantity } = req.body;
+    res.send('Order created');
+});
+
+app.get('/orders', (req: Request, res: Response) => {
+    res.send('List of orders');
+});
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
